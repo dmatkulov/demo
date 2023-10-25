84c88713c6d7bde128c2de805313c95d85dcdbde
Dilshad Matkulov <dilshadmatkulov@gmail.com>
2023-10-25 20:09:04 +0600

    webinar

--- src/App.tsx
+++ src/App.tsx
@@ -18,45 +18,62 @@ function App() {
   ]);
 
   const [showPeople, setShowPeople] = useState(true);
-  const changeName = (event: React.ChangeEvent<HTMLInputElement>, index: number) => {
-    const peopleCopy = [...people];
-    const personCopy = {...peopleCopy[index]};
-    personCopy.name = event.target.value;
-    peopleCopy[index] = personCopy;
-    setPeople(peopleCopy);
+
+  const changeName = (event: React.ChangeEvent<HTMLInputElement>, id: number) => {
+    const newName = event.target.value;
+
+    setPeople((prevState) => prevState.map((person) => {
+        if (person.id === id) {
+          return {...person, name: newName};
+        }
+        return person;
+      }));
   };
   const changeAge = () => {
-    const peopleCopy = people.map(person => {
+    setPeople((prevState) => prevState.map(person => {
       return {...person, age: person.age + 1};
-    });
-
-    console.log(peopleCopy);
-    setPeople(peopleCopy);
+    }));
   };
-  const increaseAge = (index: number) => {
-    console.log('clicked!', index);
-    const peopleCopy = [...people];
-    const personCopy = {...peopleCopy[index]};
-    personCopy.age++;
-    peopleCopy[index] = personCopy;
-    setPeople(peopleCopy);
+
+
+  const increaseAge = (id: number) => {
+    setPeople((prevState) => prevState.map((person) => {
+      if (person.id === id) {
+        return {...person, age: person.age + 1};
+      }
+      return person;
+    }));
   };
 
   const togglePeople = () => {
     setShowPeople((prevState) => !prevState);
   };
 
+  const deletePerson = (id: number) => {
+    setPeople((prevState) => prevState.filter(person => {
+      return person.id !== id;
+    }));
+  };
+
+  const addPerson = () => {
+    setPeople((prevState) => {
+      const id = prevState[prevState.length - 1].id + 1;
+      return [...prevState, {name: 'Me', age: 33, hobby: 'Nothing', id}];
+    });
+  };
+
   let peopleList: React.ReactNode = null;
 
   if (showPeople) {
-    peopleList = people.map((person, index) => {
+    peopleList = people.map((person) => {
       return (
         <Person
           key={person.id}
           name={person.name}
           age={person.age}
-          onNameClick={() => increaseAge(index)}
-          onNameChange={(event) => changeName(event, index)}
+          onNameClick={() => increaseAge(person.id)}
+          onNameChange={(event) => changeName(event, person.id)}
+          onDelete={() => deletePerson(person.id)}
         >
           <strong>Hobby: </strong>{person.hobby}
         </Person>
@@ -70,6 +87,7 @@ function App() {
       <div>
         <button onClick={changeAge}>Change age</button>
         <button onClick={togglePeople}>Toggle people</button>
+        <button onClick={addPerson}>Add Person</button>
       </div>
     </div>
   );
--- src/Person/Person.tsx
+++ src/Person/Person.tsx
@@ -5,15 +5,17 @@ interface Props extends React.PropsWithChildren {
   age: number;
   onNameClick: React.MouseEventHandler;
   onNameChange?: React.ChangeEventHandler<HTMLInputElement>;
+  onDelete: React.MouseEventHandler;
 }
 
-const Person: React.FC<Props> = ({name, age, onNameClick, onNameChange, children}) => {
+const Person: React.FC<Props> = ({name, age, onDelete, onNameClick, onNameChange, children}) => {
   return (
     <div className="person">
       <h1 onClick={onNameClick}>{name}</h1>
       <p>Age: {age}</p>
       <p>{children}</p>
       <input value={name} onChange={onNameChange}/>
+      <button onClick={onDelete}>Delete</button>
     </div>
   );
 };
